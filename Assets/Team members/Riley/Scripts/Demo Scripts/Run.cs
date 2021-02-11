09289a9ffe279328d81b03b2838e2522d5f748ea
Riley McGowan <1017093@student.sae.edu.au>
2021-02-11 16:13:35 +1100

    Personal Folder and BT Demo

---
+++ Assets/Team members/Riley/Scripts/Demo Scripts/Run.cs
@@ -0,0 +1,23 @@
+﻿using Anthill.AI;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Run : AntAIState
+{
+    public override void Enter()
+    {
+        base.Enter();
+        Debug.Log("Run Away!");
+    }
+
+    public override void Execute(float aDeltaTime, float aTimeScale)
+    {
+        base.Execute(aDeltaTime, aTimeScale);
+    }
+
+    public override void Exit()
+    {
+        base.Exit();
+    }
+}
