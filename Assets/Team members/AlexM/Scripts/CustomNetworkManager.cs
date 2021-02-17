672401ee70accde34aebb6a23159548f55ce30de
slexx <slex-@hotmail.com>
2021-02-17 16:34:10 +1100

    Early Push for saving

---
+++ Assets/Team members/AlexM/Scripts/CustomNetworkManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Mirror;
+using UnityEngine;
+
+public class CustomNetworkManager : NetworkManager
+{
+   [Header(" ")]
+   public Transform SpawnPosition;
+   public override void OnServerAddPlayer(NetworkConnection conn)
+   {
+      Transform start = SpawnPosition;
+      GameObject player = Instantiate(playerPrefab, start.position, start.rotation);
+      NetworkServer.AddPlayerForConnection(conn, player);
+   }
+
+   public override void OnServerDisconnect(NetworkConnection conn)
+   {
+      
+   }
+}
