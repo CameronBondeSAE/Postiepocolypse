f06bdb0d83e00f2df5efc70e98b2ee564218ed6f
Riley McGowan <1017093@student.sae.edu.au>
2021-02-18 10:44:33 +1100

    Behaviour Tree Script Reference Update

---
+++ Assets/Team members/Riley/Scripts/Demo Scripts/MonsterScare.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MonsterScare : MonoBehaviour
+{
+    public Transform player;
+    private Vector3 playerTransform;
+    public float distanceToPlayer;
+    public bool isScared;
+    void Update()
+    {
+        if (isScared != true)
+        {
+            playerTransform = new Vector3(player.transform.position.x, this.transform.position.y, player.transform.position.z);
+            transform.LookAt(playerTransform);
+        }
+        distanceToPlayer = Vector3.Distance(transform.position, player.transform.position);
+    }
+}
