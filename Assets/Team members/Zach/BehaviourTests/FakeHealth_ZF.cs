1518425c49ec81ac4a1c2b6a250ee3fca2c689ec
Zachariah <1019559@student.sae.edu.au>
2021-02-17 16:31:08 +1100

    Creation of Form AntAi brain
    Attempt of recreating the Form's brain in the AntAi Canvas

---
+++ Assets/Team members/Zach/BehaviourTests/FakeHealth_ZF.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FakeHealth_ZF : MonoBehaviour
+{
+    public int amount;
+
+
+    public void HealthYes()
+    {
+        Debug.Log("Yes Health!");
+    }
+}
+
