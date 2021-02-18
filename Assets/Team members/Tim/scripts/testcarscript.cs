571712d691a71a519dbc70363e07c3875c4a42b2
tim pearson <1018360@student.sae.edu.au>
2021-02-18 14:46:02 +1100

    creation of car prefab and folders

---
+++ Assets/Team members/Tim/scripts/testcarscript.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class testcarscript : MonoBehaviour
+{
+    public Rigidbody rb;
+    public float Speed;
+    public float Turn;
+    public Vector3 localVelocity;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        localVelocity = transform.InverseTransformDirection(rb.velocity);
+        rb.AddRelativeForce(-localVelocity.x,0,0);
+        if (InputSystem.GetDevice<Keyboard>().wKey.isPressed)
+        {
+            rb.AddRelativeForce(Speed,0,0);
+        }
+        if (InputSystem.GetDevice<Keyboard>().sKey.isPressed)
+        {
+            rb.AddRelativeForce(-Speed,0,0);
+        }
+        if (InputSystem.GetDevice<Keyboard>().aKey.isPressed)
+        {
+            rb.AddRelativeTorque(0,Turn,0);
+        }
+        if (InputSystem.GetDevice<Keyboard>().dKey.isPressed)
+        {
+            rb.AddRelativeTorque(0,-Turn,0);
+        }
+    }
+}
