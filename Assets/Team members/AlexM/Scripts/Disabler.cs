7734f1b5b75a4969dddd9e6d42a9784eef5da863
slexx <slex-@hotmail.com>
2021-02-19 19:03:34 +1100

    Re-Added FPS controller, Still needs to be networked!

---
+++ Assets/Team members/AlexM/Scripts/Disabler.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Disabler : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        gameObject.SetActive(false);
+    }
+}
