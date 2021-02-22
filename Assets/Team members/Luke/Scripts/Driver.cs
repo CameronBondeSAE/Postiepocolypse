a2dddc99088c7ed58a9630f8dc2cbe4e48ef9b55
Luke <1019817@student.sae.edu.au>
2021-02-22 23:47:12 +1100

    not quite finished car

--- Assets/Team members/Luke/Scripts/Driver.cs
+++ Assets/Team members/Luke/Scripts/Driver.cs
@@ -1,74 +1,112 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using Sirenix.Serialization;
+using UnityEditor.UIElements;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
-public class Driver : MonoBehaviour
+namespace Luke
 {
-    public Rigidbody rb;
-    public float speed;
-    public float reverseSpeed;
-    public float torque;
-    public Vector3 localVelocity;
-    public float rayLength;
-    public LayerMask layer;
-    public float maxSpringForce;
-    public float springResistance;
-    public Transform[] Wheels;
-    
-    void Update()
+    public class Driver : MonoBehaviour
     {
-        //to keep off the ground 
-        RaycastOut();
+        public Rigidbody rb;
+        public float speed;
+        public float reverseSpeed;
+        public Vector3 localVelocity;
+        public float springRayLength;
+        public float maxSpringForce;
+        public CurveField springForceCurve;
+        public float springResistance;
+        public float wheelFriction;
+        public float steering;
+        public float steeringAngle;
+        public Transform[] wheels;
+        public Transform[] steeringWheels;
 
-        //to get the current relative force and apply it to the x axis
-        localVelocity = transform.InverseTransformDirection(rb.velocity);
-        rb.AddRelativeForce(-localVelocity.x,0,0);
-
-        if (InputSystem.GetDevice<Keyboard>().wKey.isPressed)
+        void Update()
         {
-            rb.AddRelativeForce(0,0,speed);
-        }
-        
-        if (InputSystem.GetDevice<Keyboard>().sKey.isPressed)
-        {
-            rb.AddRelativeForce(0,0,-reverseSpeed);
+            CarMovement();
+            Springs();
         }
 
-        if (InputSystem.GetDevice<Keyboard>().aKey.isPressed)
+        public void CarMovement()
         {
-            rb.AddRelativeTorque(0,-torque,0);
-        }
-        
-        if (InputSystem.GetDevice<Keyboard>().dKey.isPressed)
-        {
-            rb.AddRelativeTorque(0,torque,0);
-        }
-    }
-
-    public void RaycastOut()
-    {
-        //ray stuff
-        foreach (Transform wheel in Wheels)
-        {
-            Ray ray = new Ray();
-            ray.origin = wheel.transform.position;
-            ray.direction = -wheel.transform.up;
-            RaycastHit raycastHit = new RaycastHit();
-            
-            Debug.DrawRay(wheel.transform.position,-wheel.transform.up,Color.yellow);
-            
-            if (Physics.Raycast(ray, out raycastHit, rayLength, layer))
+            foreach (Transform wheel in wheels)
             {
-                //apply spring resistance here
-                float force = springResistance - rayLength;
-                force *= maxSpringForce;
+                Vector3 wheelPos = wheel.transform.position;
+                
+                //getting the local velocity to apply it as a lateral friction
+                localVelocity = wheel.transform.InverseTransformDirection(rb.velocity);
+                rb.AddRelativeForce(-localVelocity.x,0,0);
 
-                rb.AddForceAtPosition(transform.up * force, wheel.transform.position);
+                //Acceleration
+                if (InputSystem.GetDevice<Keyboard>().wKey.isPressed)
+                {
+                    rb.AddForceAtPosition(wheel.transform.InverseTransformDirection(wheel.transform.forward) * speed,
+                        wheelPos);
+
+                    //wheel friction
+                    rb.AddForceAtPosition(localVelocity * -wheelFriction,
+                        wheelPos);
+                }
+
+                //Reverse
+                if (InputSystem.GetDevice<Keyboard>().sKey.isPressed)
+                {
+                    rb.AddForceAtPosition(wheel.transform.InverseTransformDirection(-wheel.transform.forward) * reverseSpeed,
+                        wheelPos);
+
+                    //wheel friction
+                    rb.AddForceAtPosition(localVelocity * wheelFriction,
+                        wheelPos);
+                }
+
+                //Steering left
+                if (InputSystem.GetDevice<Keyboard>().aKey.isPressed)
+                {
+                    //steering front wheels
+                    foreach (Transform steeringWheel in steeringWheels)
+                    {
+                        steeringWheel.transform.localRotation = Quaternion.Euler(0,-steeringAngle,0);
+                    }
+                }
+
+                //Steering right
+                if (InputSystem.GetDevice<Keyboard>().dKey.isPressed)
+                {
+                    //steering front wheels
+                    foreach (Transform steeringWheel in steeringWheels)
+                    {
+                        steeringWheel.transform.localRotation = Quaternion.Euler(0,steeringAngle,0);
+                    }
+                }
             }
         }
 
-        
+
+        //raycast system on wheels and Spring functionality
+        public void Springs()
+        {
+            foreach (Transform wheel in wheels)
+            {
+                //ray stuff
+                RaycastHit hitInfo = new RaycastHit();
+
+                //not sure how to get a curve of the spring strength going
+                float springDistance = Vector3.Distance(wheel.transform.position, hitInfo.point);
+                float force = springResistance - springDistance;
+                force *= maxSpringForce;
+
+                //this is where the spring happens
+                //255 in the layer means everything
+                if (Physics.Raycast(wheel.transform.position,-wheel.transform.up, out hitInfo,springRayLength , 255))
+                {
+                    Debug.DrawLine(wheel.transform.position,hitInfo.point,Color.yellow);
+                    rb.AddForceAtPosition(transform.InverseTransformDirection(wheel.transform.up) * force,
+                        wheel.transform.position);
+                }
+            }
+        }
     }
 }
--- Assets/Team members/Luke/Scripts/WiggleFly.cs
+++ Assets/Team members/Luke/Scripts/WiggleFly.cs
@@ -3,40 +3,43 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
-public class WiggleFly : MonoBehaviour
+namespace Luke
 {
-    public Rigidbody rb;
-    public float speed;
-    public float reverseSpeed;
-    public float torque;
-    public Vector3 localVelocity;
-
-    // Update is called once per frame
-    void Update()
+    public class WiggleFly : MonoBehaviour
     {
-        localVelocity = transform.InverseTransformDirection(rb.velocity);
+        public Rigidbody rb;
+        public float speed;
+        public float reverseSpeed;
+        public float torque;
+        public Vector3 localVelocity;
 
-        if (InputSystem.GetDevice<Keyboard>().wKey.isPressed)
+        // Update is called once per frame
+        void Update()
         {
-            rb.AddRelativeForce(0,0,speed);
-        }
-        
-        if (InputSystem.GetDevice<Keyboard>().sKey.isPressed)
-        {
-            rb.AddRelativeForce(0,0,-reverseSpeed);
-        }
+            localVelocity = transform.InverseTransformDirection(rb.velocity);
 
-        if (InputSystem.GetDevice<Keyboard>().aKey.isPressed)
-        {
-            rb.AddRelativeTorque(0,-torque,0);
-            rb.AddRelativeForce(0,-localVelocity.x,0);
-        }
-        
-        if (InputSystem.GetDevice<Keyboard>().dKey.isPressed)
-        {
-            rb.AddRelativeTorque(0,torque,0);
-            rb.AddRelativeForce(0,localVelocity.x,0);
-            
+            if (InputSystem.GetDevice<Keyboard>().wKey.isPressed)
+            {
+                rb.AddRelativeForce(0, 0, speed);
+            }
+
+            if (InputSystem.GetDevice<Keyboard>().sKey.isPressed)
+            {
+                rb.AddRelativeForce(0, 0, -reverseSpeed);
+            }
+
+            if (InputSystem.GetDevice<Keyboard>().aKey.isPressed)
+            {
+                rb.AddRelativeTorque(0, -torque, 0);
+                rb.AddRelativeForce(0, -localVelocity.x, 0);
+            }
+
+            if (InputSystem.GetDevice<Keyboard>().dKey.isPressed)
+            {
+                rb.AddRelativeTorque(0, torque, 0);
+                rb.AddRelativeForce(0, localVelocity.x, 0);
+
+            }
         }
     }
 }
