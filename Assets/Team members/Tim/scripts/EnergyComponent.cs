f40815d2711a6625007ee0888df37988ea9c6733
tim pearson <1018360@student.sae.edu.au>
2021-02-24 16:29:54 +1100

    Created energy script

---
+++ Assets/Team members/Tim/scripts/EnergyComponent.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnergyComponent : MonoBehaviour
+{
+    [Range(0f,100f)]
+    public float Amount;
+        
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
