4c36e974fca304c6b24f7a87b0c29fb1ca8971ca
John <1019178@student.sae.edu.au>
2021-02-24 14:40:09 +1100

    Car Textures

--- Assets/Team members/John M/Scripts/CarDemoJM.cs
+++ Assets/Team members/John M/Scripts/CarDemoJM.cs
@@ -1,44 +1,59 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
-public class CarDemoJM : MonoBehaviour
-{
-    private Rigidbody rb;
 
-    public Vector3 carSpeed;
-    public Vector3 turnSpeed;
-
-    // Start is called before the first frame update
-    void Start()
+namespace John
     {
-        rb = GetComponent<Rigidbody>();
+
+        public class CarDemoJM : MonoBehaviour
+        {
+            private Rigidbody rb;
+
+            public Vector3 carSpeed;
+            public Vector3 turnSpeed;
+            public Vector3 localVelocity;
+            
+            void Start()
+            {
+                rb = GetComponent<Rigidbody>();
+            }
+
+            private void Update()
+            {
+                Driving();
+            }
+
+            // Update is called once per frame
+            public void Driving()
+            {
+                //forward movement
+                 if (InputSystem.GetDevice<Keyboard>().wKey.isPressed)
+                 {
+                            rb.AddRelativeForce(carSpeed);
+                 }
+                //reverse / braking
+                 if (InputSystem.GetDevice<Keyboard>().sKey.isPressed)
+                 {
+                            rb.AddRelativeForce(-carSpeed);
+                 }
+                //turning inputs
+                 if (InputSystem.GetDevice<Keyboard>().aKey.isPressed)
+                 {
+                            rb.AddRelativeTorque(-turnSpeed);
+                 }
+
+                 if (InputSystem.GetDevice<Keyboard>().dKey.isPressed)
+                 {
+                            rb.AddRelativeTorque(turnSpeed);
+
+                 }
+            }
+        }
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        if (InputSystem.GetDevice<Keyboard>().wKey.isPressed)
-        {
-            rb.AddRelativeForce(carSpeed);
-        }
+    
 
-        if (InputSystem.GetDevice<Keyboard>().sKey.isPressed)
-        {
-                rb.AddRelativeForce(-carSpeed); 
-        }
-
-        if (InputSystem.GetDevice<Keyboard>().aKey.isPressed)
-        {
-                rb.AddRelativeTorque(-turnSpeed); 
-        }
-
-        if (InputSystem.GetDevice<Keyboard>().dKey.isPressed)
-        {
-                rb.AddRelativeTorque(turnSpeed); 
-        }
-        
-    }
-}
     
