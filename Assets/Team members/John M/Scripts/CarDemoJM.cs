8d29a25deb4c80c797a99e12debcb2a0f4226604
John <1019178@student.sae.edu.au>
2021-02-24 13:08:57 +1100

    Car Update

---
+++ Assets/Team members/John M/Scripts/CarDemoJM.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class CarDemoJM : MonoBehaviour
+{
+    private Rigidbody rb;
+
+    public Vector3 carSpeed;
+    public Vector3 turnSpeed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (InputSystem.GetDevice<Keyboard>().wKey.isPressed)
+        {
+            rb.AddRelativeForce(carSpeed);
+        }
+
+        if (InputSystem.GetDevice<Keyboard>().sKey.isPressed)
+        {
+                rb.AddRelativeForce(-carSpeed); 
+        }
+
+        if (InputSystem.GetDevice<Keyboard>().aKey.isPressed)
+        {
+                rb.AddRelativeTorque(-turnSpeed); 
+        }
+
+        if (InputSystem.GetDevice<Keyboard>().dKey.isPressed)
+        {
+                rb.AddRelativeTorque(turnSpeed); 
+        }
+        
+    }
+}
+    
