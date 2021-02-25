237daca7f505f0957f4d78e1d51ae68f420553ba
John <1019178@student.sae.edu.au>
2021-02-25 20:27:02 +1100

    Inventory, Coin and Item Code Added
    inventory can be filled with items.
    coin counter increases on collection of coins
    items can be added and removed from inventory display on screen
    new items can easily be added and coded through "Item pick up" script

--- Assets/Team members/John M/Scripts/Inventory.cs
+++ Assets/Team members/John M/Scripts/Inventory.cs
@@ -1,22 +1,68 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
+using UnityEngine.UI;
 
-namespace John
+namespace JonathonMiles
 {
 
     public class Inventory : MonoBehaviour
     {
-        // Start is called before the first frame update
-        void Start()
+        #region Singleton
+        public static Inventory instance;
+
+        private void Awake()
+        {
+            if (instance != null)
+            {
+                Debug.Log("More than one instance of Inventory found !!");
+                return;
+            }
+            instance = this;
+        }
+        
+
+        #endregion
+
+        public delegate void OnItemChanged();
+
+        public OnItemChanged onItemChangedCallBack;
+        
+        public int inventorySpace = 2;
+        public int coinCount = 0;
+        public Text coinText;
+        public List<ItemBase> items = new List<ItemBase>();
+        
+
+        public bool Add(ItemBase item)
         {
 
+            if (item.isCoin)
+            {
+                coinCount = coinCount + item.amount;
+                Debug.Log("You got a coin!");
+                coinText.text = ("x " + coinCount);
+                return true;
+            }
+            if (items.Count >= inventorySpace)
+            {
+                Debug.Log("Inventory Full!");
+                return false;
+            }
+            
+            items.Add(item);
+            if (onItemChangedCallBack != null)
+                onItemChangedCallBack.Invoke();
+            return true;
         }
 
-        // Update is called once per frame
-        void Update()
+        public void Remove(ItemBase item)
         {
-
+            items.Remove(item);
+            if (onItemChangedCallBack != null)
+                onItemChangedCallBack.Invoke();
         }
     }
 }
