c35a9134552a6f5b6f9b8d5cf4388b9b2fb3d24f
Luke <1019817@student.sae.edu.au>
2021-02-25 12:22:58 +1100

    creaturemanager start

--- Assets/Team members/Luke/Scripts/CreatureManager.cs
+++ Assets/Team members/Luke/Scripts/CreatureManager.cs
@@ -2,17 +2,20 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CreatureManager : MonoBehaviour
+namespace Luke
 {
-    // Start is called before the first frame update
-    void Start()
+    public class CreatureManager : MonoBehaviour
     {
-        
-    }
+        // Start is called before the first frame update
+        void Start()
+        {
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
     }
 }
