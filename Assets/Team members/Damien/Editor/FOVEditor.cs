222952543c054b67c4ade340dfc3792faa3c05c8
Damien Harwood <1019243@student.sae.edu.au>
2021-03-03 16:18:21 +1100

    FOV work

---
+++ Assets/Team members/Damien/Editor/FOVEditor.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using Damien;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.XR;
+
+[CustomEditor(typeof(FOV))]
+public class FOVEditor : Editor
+{
+    private void OnSceneGUI()
+    {
+        FOV fov = (FOV)target;
+        Handles.color = Color.white;
+        Handles.DrawWireArc(fov.transform.position, Vector3.up, Vector3.forward, 360, fov.viewRadius);
+    }
+}
