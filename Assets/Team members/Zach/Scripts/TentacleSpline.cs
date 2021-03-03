4374c509d88042df92909adda4e0bbc8cac2bc52
Zachariah <1019559@student.sae.edu.au>
2021-03-03 14:39:02 +1100

    Smell Start of Level 2 and some Tentacle stuff

---
+++ Assets/Team members/Zach/Scripts/TentacleSpline.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using SplineMesh;
+using UnityEngine;
+
+
+namespace ZachFrench
+{
+
+    public class TentacleSpline : MonoBehaviour
+    {
+
+        public Spline spline;
+        public float perlinX;
+        public float perlinY;
+        public float perlinZ;
+        public Vector3 splinePosition;
+        public Vector3 splineDirection;
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            spline.nodes.Add(new SplineNode(splinePosition,splineDirection));
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            perlinX = Mathf.PerlinNoise(Random.Range(0f, 10f),Random.Range(0f, 10f));
+            perlinY = Mathf.PerlinNoise(Random.Range(0f, 10f),Random.Range(0f, 10f));
+            perlinZ = Mathf.PerlinNoise(Random.Range(0f, 10f),Random.Range(0f, 10f));
+            splinePosition = new Vector3(perlinX,perlinY,perlinZ);
+            splineDirection = new Vector3(perlinX,perlinY,perlinZ);
+        }
+    }
+}
