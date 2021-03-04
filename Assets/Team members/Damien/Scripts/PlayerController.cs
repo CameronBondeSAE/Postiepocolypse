103258a18b3f6c64496a95c992f99557d0fc63e8
Damien Harwood <1019243@student.sae.edu.au>
2021-03-04 16:58:14 +1100

    Just some portal window ads
    Ill get this working soon and hopefully we can use this for the in game portals :)

---
+++ Assets/Team members/Damien/Scripts/PlayerController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Damien
+{
+    public class PlayerController : MonoBehaviour
+    {
+
+        public CharacterController controller;
+
+        public float movementSpeed = 3f;
+
+        // Update is called once per frame
+        void Update()
+        {
+            float x = Input.GetAxis("Horizontal");
+            float z = Input.GetAxis("Vertical");
+
+            Vector3 move = transform.right * x + transform.forward * z;
+
+            controller.Move(move * (movementSpeed * Time.deltaTime));
+        }
+    }
+}
---
+++ Assets/Team members/Damien/Scripts/PortalTeleporter.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Damien
+{
+    public class PortalTeleporter : MonoBehaviour
+    {
+        public Transform player;
+
+        public Transform reciever;
+
+        private bool playerIsOverlapping = false;
+
+        public float dotProduct;
+
+        public Vector3 portalToPlayer;
+
+        void Update()
+        {
+            portalToPlayer = player.position - transform.position;
+            dotProduct = Vector3.Dot(transform.up, portalToPlayer);
+
+            if (playerIsOverlapping)
+            {
+                if (dotProduct < 0f)
+                {
+                    //check current rotation and compare to end portal rotation.
+                    float rotationDiff = -Quaternion.Angle(transform.rotation, reciever.rotation);
+                    rotationDiff += 180f;
+                    player.Rotate(Vector3.up, rotationDiff);
+
+                    Vector3 positionOffset = Quaternion.Euler(0f, rotationDiff, 0f) * portalToPlayer;
+                    player.position = reciever.position + positionOffset;
+
+                    playerIsOverlapping = false;
+                }
+            }
+        }
+
+        private void OnTriggerEnter(Collider other)
+        {
+            if (other.tag == "Player")
+            {
+                playerIsOverlapping = true;
+                Debug.Log("Portal ON");
+            }
+        }
+
+        private void OnTriggerExit(Collider other)
+        {
+            if (other.tag == "Player")
+            {
+                playerIsOverlapping = false;
+                Debug.Log("Portal OFF");
+            }
+        }
+    }
+}
