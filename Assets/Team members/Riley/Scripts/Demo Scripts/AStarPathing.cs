ac78dd0336ff79a8456e885b8cf32dd1dfa30727
Riley McGowan <1017093@student.sae.edu.au>
2021-03-10 16:57:31 +1100

    AStar lists
    Basically has all of the setup for the water pathfinding example without gizmos drawn yet.

---
+++ Assets/Team members/Riley/Scripts/Demo Scripts/AStarPathing.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using RileyMcGowan;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+namespace RileyMcGowan
+{
+    public class AStarPathing : MonoBehaviour
+    {
+        public WorldScanner worldScanner;
+        public List<Node> Open;
+        public List<Node> Closed;
+        public Node currentNode;
+        public Node workingNode;
+        public Vector2 currentPosition;
+        
+        void Start()
+        {
+            Open.Add(worldScanner.gridList[Random.Range(0, worldScanner.gridSize.x), Random.Range(0, worldScanner.gridSize.z)]);
+        }
+        
+        void Update()
+        {
+            currentNode = Open[Random.Range(0, Open.Count)];
+            Open.Remove(currentNode);
+            Closed.Add(currentNode);
+
+            for (int x = -1; x < 2; x++)
+            {
+                for (int z = -1; z < 2; z++)
+                {
+                    workingNode = worldScanner.gridList[currentNode.nodePos.x + x, currentNode.nodePos.y + z]; //Used for working with the current node without rewriting
+                    if (workingNode != null && Closed.Contains(workingNode) != true && Open.Contains(workingNode))
+                    {
+                        Open.Add(workingNode);
+                    }
+                }
+            }
+        }
+
+        private void OnDrawGizmos()
+        {
+            //Gizmos.DrawCube();
+        }
+    }
+}
--- Assets/Team members/Riley/Scripts/Demo Scripts/WorldScanner.cs
+++ Assets/Team members/Riley/Scripts/Demo Scripts/WorldScanner.cs
@@ -5,28 +5,27 @@ using UnityEngine;
 
 namespace RileyMcGowan
 {
+    [Serializable]
     public class Node
     {
         public bool objectFound;
+        public Vector2Int nodePos;
     }
 
-    public class AStarPathfinding : MonoBehaviour
+    public class WorldScanner : MonoBehaviour
     {
         public Vector3Int gridSize;
         
         public Node[,] gridList;
         [Tooltip("By Default this should be set to 1")]
         public Vector3 gridBoxSize;
-
         public Vector3 yaBoi;
 
-        // Start is called before the first frame update
         void Start()
         {
             gridList = new Node[gridSize.x, gridSize.z];
         }
 
-        // Update is called once per frame
         void Update()
         {
             GridWorldLayout();
@@ -39,7 +38,8 @@ namespace RileyMcGowan
                 for (int z = 0; z < gridSize.z; z++)
                 {
                     gridList[x, z] = new Node();
-                    gridList[x, z].objectFound = false;                    
+                    gridList[x, z].objectFound = false;
+                    gridList[x, z].nodePos = new Vector2Int(x, z);
                     yaBoi = transform.localPosition;
                     if (Physics.CheckBox(new Vector3(x * gridBoxSize.x, 0, z * gridBoxSize.y), gridBoxSize))
                     {
@@ -47,18 +47,6 @@ namespace RileyMcGowan
                     }
                 }
             }
-
-            /*for (int x = 0; x < gridSize.x; x++)
-            {
-                for (int z = 0; z < gridSize.z; z++)
-                {
-                    if (Physics.OverlapBox(new Vector3(x*gridSize.x, 0, z*gridSize.z), new Vector3(gridSize.x, gridSize.y, gridSize.z), Quaternion.identity))
-                    {
-                        // Something is there
-                        grid[x, y].isBlocked = true;
-                    }
-                }
-            }*/
         }
 
         private void OnDrawGizmos()
@@ -67,7 +55,7 @@ namespace RileyMcGowan
             {
                 for (int z = 0; z < gridSize.z; z++)
                 {
-                    if (gridList[x, z].objectFound && gridList[x,z] != null)
+                    if (gridList[x, z].objectFound && gridList != null && gridList[x, z] != null)
                     {
                         Gizmos.color = Color.black;
                         Gizmos.DrawWireCube(new Vector3(x * gridBoxSize.x, 0, z * gridBoxSize.y), gridBoxSize);
