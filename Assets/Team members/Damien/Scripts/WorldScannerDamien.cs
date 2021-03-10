c1fd71fa481ac12205bd601ecf08578d363c9b3a
Damien Harwood <1019243@student.sae.edu.au>
2021-03-11 00:53:29 +1100

    WorldScanner

---
+++ Assets/Team members/Damien/Scripts/WorldScannerDamien.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Damien
+{
+    public class WorldScannerDamien : MonoBehaviour
+    {
+        public class Point
+        {
+            private bool thingFound;
+        }
+        
+        public Vector3Int gridSize;
+        
+    }
+}
