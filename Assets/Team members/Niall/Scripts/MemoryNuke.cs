ab22bb0234201c5c8fb4b50ddc1f093f56c34f5f
Niall <1013734@student.sae.edu.au>
2021-03-11 14:15:56 +1100

    resource spawning begun

---
+++ Assets/Team members/Niall/Scripts/MemoryNuke.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering.UI;
+
+namespace Niall
+{
+    public class MemoryNuke : MonoBehaviour
+    {
+        private int power = 100;
+        private GameObject go;
+        private int t;
+        void Update()
+        {
+            for (int i = 0; i < power; i++)
+            {
+                t++;
+                new GameObject();
+                Debug.Log("ITEM" + t);
+
+            }
+            
+            Collect();
+            
+        }
+
+        void Collect()
+        {
+            if (Input.GetKeyUp(KeyCode.Space))
+            {
+                GC.Collect();
+                Debug.Log("GC.Collect initiated.");
+            }
+        }
+    }
+}
---
+++ Assets/Team members/Niall/Scripts/ResourceSpawner.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Luke;
+using UnityEngine;
+
+namespace Niall
+{
+    public class ResourceSpawner : MonoBehaviour
+    {
+        public int resources = 5;
+        public GameObject resource;
+        private float RanX;
+        private float RanZ;
+
+        void Update()
+        {
+            RanX = Random.Range(-10, 10);
+            RanZ = Random.Range(-10, 10);
+            StartCoroutine(Spawn());
+        }
+
+        IEnumerator Spawn()
+        {
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                for (int i = 0; i < resources; i++)
+                {
+                    Instantiate(resource, new Vector3(RanX, 1, RanZ), Quaternion.identity);
+                    yield return new WaitForSeconds(0);
+
+                }
+            }
+        }
+ 
+    }
+}
