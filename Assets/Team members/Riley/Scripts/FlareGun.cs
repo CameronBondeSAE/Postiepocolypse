0938f31c12c778c4fd60458e31e1b19db64f13c5
Riley McGowan <1017093@student.sae.edu.au>
2021-03-11 15:40:08 +1100

    Flare Changes
    Made the flare an individual object and the flaregun is a different script.

--- Assets/Team members/Riley/Scripts/FlareGun.cs
+++ Assets/Team members/Riley/Scripts/FlareGun.cs
@@ -12,18 +12,20 @@ namespace RileyMcGowan
         private GameObject instantiate;
         private Vector3 spawnLocation;
         private Vector3 flareDirection;
-
+        private bool isCoroutineRunning;
+        private Rigidbody currentFlareRigidbody;
+        
         public GameObject objectToSpawn;
         [Tooltip("Is a flare ready.")]
         public bool flareAvailable;
 
-        [Tooltip("Delay between flares. Expected 1-30.")]
+        [Tooltip("Delay between flares. Expects 1-30.")]
         public int flareDelay;
 
-        [Tooltip("The distance to spawn the flare from player. Works at 2.")]
+        [Tooltip("The distance to spawn the flare from player. Expects 1-3.")]
         public float spawnDifference;
 
-        [Tooltip("Force to fire flare. Works at 5.")]
+        [Tooltip("Force to fire flare. Expects 1-5.")]
         public int flareForce;
 
         [Tooltip("Changeable by other scripts.")]
@@ -31,7 +33,10 @@ namespace RileyMcGowan
 
         [Tooltip("Height for flare to travel at max. Works at 100.")]
         public int flareHeight;
-
+        
+        [Tooltip("Flare Drag. Expects between 1-10.")]
+        public int flareDecent = 10;
+        
         private void Start()
         {
             StartCoroutine(FlareDelay());
@@ -40,6 +45,11 @@ namespace RileyMcGowan
         void Update()
         {
             FireFlare();
+            if (flareAvailable == true && isCoroutineRunning == true)
+            {
+                StopCoroutine(FlareDelay());
+                isCoroutineRunning = false;
+            }
         }
 
         public void FireFlare()
@@ -49,18 +59,27 @@ namespace RileyMcGowan
                 spawnLocation = new Vector3(transform.position.x, transform.position.y + spawnDifference, transform.position.z + spawnDifference / 2);
                 flareDirection = new Vector3(transform.position.x, transform.position.y + flareHeight, transform.position.z); //Where to fire the flare
                 instantiate = Instantiate<GameObject>(objectToSpawn, spawnLocation, this.transform.rotation); //Store our flare to influence
-                instantiate.GetComponent<Rigidbody>().AddRelativeForce(flareDirection * flareForce); //Force added to the flare
-                instantiate.GetComponent<ParticleSystem>().Play();
+                currentFlareRigidbody = instantiate.GetComponent<Rigidbody>();
+                StartCoroutine(DragController());
+                currentFlareRigidbody.AddRelativeForce(flareDirection * flareForce); //Force added to the flare
                 currentFlares -= 1; //Remove a flare from inventory
                 flareAvailable = false;
                 StartCoroutine(FlareDelay());
             }
         }
-
+        
         IEnumerator FlareDelay()
         {
+            isCoroutineRunning = true;
             yield return new WaitForSeconds(flareDelay);
             flareAvailable = true;
+            isCoroutineRunning = false;
+        }
+        IEnumerator DragController()
+        {
+            currentFlareRigidbody.drag = 1;
+            yield return new WaitForSeconds(2);
+            currentFlareRigidbody.drag = flareDecent;
         }
     }
 }
