e5f5070b580e54fdaf2af42403f405bfca7c15f9
Luke <1019817@student.sae.edu.au>
2021-03-13 18:02:46 +1100

    Motion sensor

--- Assets/Team members/Luke/Scripts/MotionSensor.cs
+++ Assets/Team members/Luke/Scripts/MotionSensor.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -6,17 +7,45 @@ namespace Luke
 {
     public class MotionSensor : MonoBehaviour
     {
+        public float pingDuration;
+        public float pingWaitTime;
+        public List<Vector3> sensedRigidbodies;
+        
+        private float maxPingWaitTime;
 
-        // Start is called before the first frame update
         void Start()
         {
-
+            maxPingWaitTime = pingWaitTime;
         }
 
-        // Update is called once per frame
-        void Update()
+        public void OnTriggerEnter(Collider other)
         {
+            sensedRigidbodies.Add(other.transform.position);
+        }
 
+        public IEnumerator OnTriggerStay(Collider other)
+        {
+            Rigidbody sensedRigidbody = other.attachedRigidbody;
+            Vector3 velocity = sensedRigidbody.velocity;
+
+            pingWaitTime -= Time.deltaTime;
+
+            if (velocity.magnitude > 0 && pingWaitTime > 0)
+            {
+                //example but this should shoot out once
+                Debug.Log("detecting Rigidbody at " + sensedRigidbody.transform.position);
+            }
+            
+            else if (pingWaitTime <= 0)
+            {
+                yield return new WaitForSeconds(pingDuration);
+                pingWaitTime = maxPingWaitTime; 
+            }
+        }
+
+        public void OnTriggerExit(Collider other)
+        {
+            sensedRigidbodies.Remove(other.transform.position);
         }
     }
 }
