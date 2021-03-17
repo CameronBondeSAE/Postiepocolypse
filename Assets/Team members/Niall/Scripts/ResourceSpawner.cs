93c64536d60b2464d061d6b6778d350095bfe10a
Niall <1013734@student.sae.edu.au>
2021-03-17 16:12:36 +1100

    resoruce spawner networking with server, yet to test player sync

--- Assets/Team members/Niall/Scripts/ResourceSpawner.cs
+++ Assets/Team members/Niall/Scripts/ResourceSpawner.cs
@@ -1,36 +1,51 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
-using Luke;
+using Mirror;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 namespace Niall
 {
-    public class ResourceSpawner : MonoBehaviour
+    public class ResourceSpawner : NetworkBehaviour
     {
         public int resources = 5;
         public GameObject resource;
         private float RanX;
         private float RanZ;
+        private float spawnTime = 1;
+        private float spawnDelay= 1;
+        public float minRange = -25;
+        public float maxRange = 25;
+
+        public override void OnStartServer()
+        {
+            StartCoroutine("Spawn");
+            base.OnStartServer();
+        }
+
 
         void Update()
         {
-            RanX = Random.Range(-10, 10);
-            RanZ = Random.Range(-10, 10);
-            StartCoroutine(Spawn());
+            RanX = Random.Range(minRange, maxRange);
+            RanZ = Random.Range(minRange, maxRange);
         }
 
         IEnumerator Spawn()
         {
-            if (Input.GetKeyDown(KeyCode.E))
+            if (isServer)
             {
                 for (int i = 0; i < resources; i++)
                 {
-                    Instantiate(resource, new Vector3(RanX, 1, RanZ), Quaternion.identity);
+                    Instantiate(resource, transform.position + new Vector3(RanX, 1, RanZ), Quaternion.identity);
                     yield return new WaitForSeconds(0);
-
                 }
             }
         }
- 
+
+        private void OnDrawGizmos()
+        {
+            Gizmos.DrawWireCube(transform.position, new Vector3(maxRange, 1, maxRange));
+        }
     }
 }
