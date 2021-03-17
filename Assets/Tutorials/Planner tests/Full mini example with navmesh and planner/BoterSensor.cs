6214c14e0e019af7a10e112ed336f9a19071e4df
Cameron Bonde <c.bonde@sae.edu>
2021-03-18 10:27:06 +1100

    namespaces

--- Assets/Tutorials/Planner tests/Full mini example with navmesh and planner/BoterSensor.cs
+++ Assets/Tutorials/Planner tests/Full mini example with navmesh and planner/BoterSensor.cs
@@ -3,15 +3,18 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class BoterSensor : MonoBehaviour, ISense
+namespace CameronBonde
 {
-	public void CollectConditions(AntAIAgent aAgent, AntAICondition aWorldState)
+	public class BoterSensor : MonoBehaviour, ISense
 	{
-		Debug.Log("Collect conditions");
+		public void CollectConditions(AntAIAgent aAgent, AntAICondition aWorldState)
+		{
+			Debug.Log("Collect conditions");
 		
-		aWorldState.BeginUpdate(aAgent.planner);
-		aWorldState.Set("Am I at the target position", false);
-		aWorldState.Set("Has target", aAgent.GetComponent<Boter_Model>().target != null);
-		aWorldState.EndUpdate();
+			aWorldState.BeginUpdate(aAgent.planner);
+			aWorldState.Set("Am I at the target position", false);
+			aWorldState.Set("Has target",                  aAgent.GetComponent<Boter_Model>().target != null);
+			aWorldState.EndUpdate();
+		}
 	}
 }
