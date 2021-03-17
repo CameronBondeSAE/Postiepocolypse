4e34479fe36940c968db0786bb1d94d0a10dc0f6
Damien Harwood <1019243@student.sae.edu.au>
2021-03-17 16:15:50 +1100

    NavMesh + Planner

---
+++ Assets/Team members/Damien/AntAI/EntitySensorDamien.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Anthill.AI;
+using UnityEngine;
+
+public class EntitySensorDamien : MonoBehaviour, ISense
+{
+    public void CollectConditions(AntAIAgent aAgent, AntAICondition aWorldState)
+    {
+        aWorldState.BeginUpdate(aAgent.planner);
+        aWorldState.Set("At Target", false);
+        aWorldState.Set("Has Target", false);
+        aWorldState.EndUpdate();
+    }
+}
---
+++ Assets/Team members/Damien/AntAI/MoveToTargetDamien.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Anthill.AI;
+using UnityEngine;
+
+public class MoveToTargetDamien : AntAIState
+{
+    public override void Enter()
+    {
+        base.Enter();
+    }
+}
