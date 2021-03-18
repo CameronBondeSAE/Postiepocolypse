e9a26ad634dc39f61b2e85efcc1e6f12ae33a026
Zachariah <1019559@student.sae.edu.au>
2021-03-18 12:40:05 +1100

    Edits and Completion of base NavMesh planner
    Requires use of the Targets prefab

--- Assets/Team members/Zach/Scripts/NavGuyStates/MoveToTarget.cs
+++ Assets/Team members/Zach/Scripts/NavGuyStates/MoveToTarget.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using Anthill.AI;
 using UnityEngine;
+using UnityEngine.AI;
 
 namespace ZachFrench
 {
@@ -9,6 +10,34 @@ namespace ZachFrench
 
     public class MoveToTarget : AntAIState
     {
+        public GameObject parent;
+        private NavMeshAgent NavMeshAgent;
 
+        public override void Create(GameObject aGameObject)
+        {
+            base.Create(aGameObject);
+            parent = aGameObject;
+            NavMeshAgent = parent.GetComponent<NavMeshAgent>();
+        }
+
+        public override void Enter()
+        {
+            base.Enter();
+
+            Debug.Log("Beginning Moving");
+
+            NavMeshAgent.SetDestination(parent.GetComponent<NavDudeBody>().target.transform.position);
+        }
+
+        public override void Execute(float aDeltaTime, float aTimeScale)
+        {
+            base.Execute(aDeltaTime, aTimeScale);
+
+            if (NavMeshAgent.remainingDistance < 1f)
+            {
+                parent.GetComponent<NavDudeBody>().target = null;
+                Finish();
+            }
+        }
     }
 }
