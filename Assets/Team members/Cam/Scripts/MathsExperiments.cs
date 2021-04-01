3e61f3b05a3aeddc6fdb9638fc6a6f0f93240e6b
Cameron Bonde <c.bonde@sae.edu>
2021-04-01 13:18:56 +1100

    Tute updates Main scene updates Player interact fixes

---
+++ Assets/Team members/Cam/Scripts/MathsExperiments.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using UnityEngine;
+
+public class MathsExperiments : MonoBehaviour
+{
+	[SerializeField]
+	int numberOfBounces = 5;
+
+	Vector3    hitPosition;
+	Vector3    reflectedDirection;
+	Ray        ray;
+	float      maxDistance;
+	RaycastHit hitInfo;
+	bool       raycast;
+
+	// List<Vector3> hitInfoPoint;
+	Vector3[] hitInfoPoint;
+
+
+	void Start()
+	{
+		maxDistance = 99999f;
+		ray         = new Ray();
+
+		// hitInfoPoint = new List<Vector3>(numberOfBounces);
+		hitInfoPoint = new Vector3[numberOfBounces];
+	}
+
+	void Update()
+	{
+		hitPosition        = transform.position;
+		reflectedDirection = transform.forward;
+		
+		Array.Clear(hitInfoPoint, 0, numberOfBounces);
+		for (int i = 0; i < numberOfBounces; i++)
+		{
+			if (!ShootRay(hitPosition, reflectedDirection, out hitPosition, out reflectedDirection, i))
+			{
+				break;
+			}
+		}
+
+		// for (int bounce = 0; bounce < numberOfBounces - 1; bounce++)
+		// {
+			// Debug.DrawLine(hitInfoPoint[bounce], hitInfoPoint[bounce + 1], new Color(0, 1f - (float) bounce / numberOfBounces, 0), 0, true);
+			// Debug.DrawLine(hitInfoPoint[bounce], hitInfoPoint[bounce + 1]);
+		// }
+	}
+
+	bool ShootRay(Vector3 origin, Vector3 direction, out Vector3 _hitPosition, out Vector3 _reflectedDirection, int bounce)
+	{
+		ray.origin    = origin;
+		ray.direction = direction;
+
+		raycast = Physics.Raycast(ray, out hitInfo, maxDistance);
+		if (raycast)
+		{
+			// hitInfoPoint[bounce] = hitInfo.point; // Debug draw caching
+			Debug.DrawLine(origin, hitInfo.point);
+			_hitPosition         = hitInfo.point;
+			_reflectedDirection  = Vector3.Reflect(direction, hitInfo.normal);
+			return true;
+		}
+		else
+		{
+			_hitPosition        = Vector3.zero;
+			_reflectedDirection = Vector3.zero;
+			return false;
+		}
+	}
+}
---
+++ Assets/Team members/Cam/Scripts/Spinner.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spinner : MonoBehaviour
+{
+	public float speed = 10f;
+
+	// Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(0,speed*Time.deltaTime,0);
+    }
+}
