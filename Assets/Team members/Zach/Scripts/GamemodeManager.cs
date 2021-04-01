7e6547a722c23d399b47b935b0b915ad20093ef7
Zachariah <1019559@student.sae.edu.au>
2021-04-01 16:42:32 +1100

    Added Respawn section of game manager
    (planned to change and update as I connect the other managers to this)

--- Assets/Team members/Zach/Scripts/GamemodeManager.cs
+++ Assets/Team members/Zach/Scripts/GamemodeManager.cs
@@ -1,18 +1,38 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using AJ;
+using Niall;
 using UnityEngine;
+using ZachFrench;
 
-public class GamemodeManager : MonoBehaviour
+namespace ZachFrench
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
+
+    public class GamemodeManager : MonoBehaviour
     {
-        
+        public RespawnManager rm;
+        public ResourceSpawner rs;
+        public Luke.Spawner hellSpawner;
+
+        public int civilianCount = 5;
+        public int playerCount = 2;
+
+        // Start is called before the first frame update
+        void Awake()
+        {
+            rm = FindObjectOfType<RespawnManager>();
+            rs = FindObjectOfType<ResourceSpawner>();
+            hellSpawner = FindObjectOfType<Luke.Spawner>();
+
+            rm.numberOfCivilian = civilianCount;
+            rm.numberOfPlayers = playerCount;
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
     }
 }
