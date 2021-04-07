be9cec79af15d47b9cf206e2f60d3c2ede5be2c8
Zachariah <1019559@student.sae.edu.au>
2021-04-07 18:02:35 +1000

    General Updates to Some code and GameManager

--- Assets/Team members/Zach/Scripts/GamemodeManager.cs
+++ Assets/Team members/Zach/Scripts/GamemodeManager.cs
@@ -11,22 +11,27 @@ namespace ZachFrench
 
     public class GamemodeManager : MonoBehaviour
     {
-        public RespawnManager rm;
-        public ResourceSpawner rs;
-        public Luke.Spawner hellSpawner;
-
+        // Make sure that there are already the other managers in the scene as these only grab them and don't spawn them
+        private RespawnManager rm;
+        private ResourceSpawner rs;
+        private Luke.Spawner hellSpawner;
+        //These are just set values for testing and can be altered in code or in the scene 
         public int civilianCount = 5;
         public int playerCount = 2;
+        public int creatureCount = 5;
 
         // Start is called before the first frame update
         void Awake()
         {
+            //Finds the other managers 
             rm = FindObjectOfType<RespawnManager>();
             rs = FindObjectOfType<ResourceSpawner>();
             hellSpawner = FindObjectOfType<Luke.Spawner>();
-
+            
+            //this sets the values within the managers to the values located in the Gamemode Manager
             rm.numberOfCivilian = civilianCount;
             rm.numberOfPlayers = playerCount;
+            hellSpawner.numberOfCreatures = creatureCount;
         }
 
         // Update is called once per frame
