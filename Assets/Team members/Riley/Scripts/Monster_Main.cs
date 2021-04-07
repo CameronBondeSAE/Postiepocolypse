c1fcc229569715219ae82de75628b7e5a9f866b2
Riley McGowan <1017093@student.sae.edu.au>
2021-04-07 16:31:23 +1000

    Fixing my mistakes

--- Assets/Team members/Riley/Scripts/Monster_Main.cs
+++ Assets/Team members/Riley/Scripts/Monster_Main.cs
@@ -16,6 +16,7 @@ namespace RileyMcGowan
         private Vector3 vectorToTarget;
         public bool invertDirection;
         private Vector3 vectorNormalised;
+        private float signedToTarget;
         private void Awake()
         {
             antAIAgent = GetComponent<AntAIAgent>();
@@ -45,7 +46,7 @@ namespace RileyMcGowan
                     Debug.Log("The vectorToTarget is " + vectorToTarget);
                     directionToTarget = Vector3.Angle(vectorToTarget, transform.forward);
                     Debug.Log("The directionToTarget is " + directionToTarget);
-                    float signedToTarget = Vector3.SignedAngle(vectorToTarget, transform.forward, transform.up);
+                    signedToTarget = Vector3.SignedAngle(vectorToTarget, transform.forward, Vector3.up);
                     Debug.Log("The Signed vector is " + signedToTarget);
                 }
             }
