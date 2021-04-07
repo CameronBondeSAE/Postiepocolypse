161d18e2a66815db78c4f03e212c40f9c5918ae5
Damien Harwood <1019243@student.sae.edu.au>
2021-04-07 21:27:32 +1000

    FOV Script Added

---
+++ Assets/Scripts/FOV.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using Tanks;
+using tripolygon.UModeler;
+using UnityEngine;
+using UnityEngine.PlayerLoop;
+
+
+namespace Damien
+{
+    public class FOV : MonoBehaviour
+    {
+        public GameObject owner;
+        public LayerMask target;
+        public LayerMask obstacle;
+
+        public float viewRadius;
+        [Range(0, 360)] public float viewAngle = 360;
+        
+        
+
+        public List<Transform> playerTargets = new List<Transform>();
+
+        private void Start()
+        {
+            StartCoroutine("SeePlayers", 0.2f);
+        }
+
+        IEnumerator SeePlayers(float delay)
+        {
+            while (true)
+            {
+                yield return new WaitForSeconds(delay);
+                DetectPlayer();
+                
+            }
+        }
+
+        void DetectPlayer()
+        {
+            playerTargets.Clear();
+            Collider[] targetsInViewRadius = Physics.OverlapSphere(transform.position, viewRadius, target);
+            for (int i = 0; i < targetsInViewRadius.Length; i++)
+            {
+                Transform target = targetsInViewRadius[i].transform;
+                Vector3 dirToTarget = (target.position - transform.position).normalized;
+                if (Vector3.Angle(transform.forward, dirToTarget) < viewAngle / 2)
+                {
+                    float distance = Vector3.Distance(transform.position, target.position);
+                    if (!Physics.Raycast(transform.position, dirToTarget, distance, obstacle))
+                    {
+                        playerTargets.Add(target);
+                    }
+                }
+            }
+        }
+
+        public Vector3 DirFromAngle(float angleInDegrees, bool angleIsGlobal)
+        {
+            if (!angleIsGlobal)
+            {
+                angleInDegrees += transform.eulerAngles.y;
+            }
+            return new Vector3(Mathf.Sin(angleInDegrees * Mathf.Deg2Rad), 0, Mathf.Cos(angleInDegrees * Mathf.Deg2Rad));
+        }
+    }
+}
--- Assets/Team members/Damien/Editor/FOVEditor.cs
+++ Assets/Team members/Damien/Editor/FOVEditor.cs
@@ -1,18 +1,30 @@
-﻿using System;
-using System.Collections;
-using System.Collections.Generic;
-using Damien;
+﻿using Damien;
 using UnityEditor;
 using UnityEngine;
-using UnityEngine.XR;
+using System.Collections;
+using TPUModelerEditor;
 
 [CustomEditor(typeof(FOV))]
 public class FOVEditor : Editor
 {
-    private void OnSceneGUI()
+    void OnSceneGUI()
     {
-        FOV fov = (FOV)target;
-        Handles.color = Color.white;
+        FOV fov = (FOV) target;
+
+        Vector3 viewAngleA = fov.DirFromAngle(-fov.viewAngle / 2, false);
+        Vector3 viewAngleB = fov.DirFromAngle(fov.viewAngle / 2, false);
+
+
+        Handles.color = Color.gray;
         Handles.DrawWireArc(fov.transform.position, Vector3.up, Vector3.forward, 360, fov.viewRadius);
+        Handles.color = Color.blue;
+        Handles.DrawLine(fov.transform.position, fov.transform.position + viewAngleA * fov.viewRadius);
+        Handles.DrawLine(fov.transform.position, fov.transform.position + viewAngleB * fov.viewRadius);
+
+        foreach (Transform playerTarget in fov.playerTargets)
+        {
+            Handles.color = Color.red;
+            Handles.DrawLine(fov.transform.position, playerTarget.position);
+        }
     }
 }
