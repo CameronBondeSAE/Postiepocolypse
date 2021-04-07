ee1f944c3cd745b553c76266faf58cb125411fa6
tim pearson <1018360@student.sae.edu.au>
2021-04-07 16:09:25 +1000

    Moved Scripts and prefabs to general use folders
    moved my energy and sprint scripts to the scripts folder and moved my sprinter prefab to the prefabs folder and also added RaycastHit code to sprinterai script

---
+++ Assets/Scripts/Sprint.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+
+namespace TimPearson
+{
+    /// <summary>
+    ///     Needs to have the Energy Script to function.
+    /// </summary>
+    public class Sprint : MonoBehaviour
+    {
+        public bool isBoosting;
+        public Rigidbody rb;
+        public float Boost;
+        public Energy energy;
+        public float Drain;
+
+
+        // Start is called before the first frame update
+        private void Start()
+        {
+            energy = GetComponent<Energy>();
+            rb = gameObject.GetComponent<Rigidbody>();
+        }
+
+        // Update is called once per frame
+        private void Update()
+        {
+            if (isBoosting)
+            {
+                // Reduce energy by the Drain amount per second
+                energy.Amount -= Drain * Time.deltaTime;
+
+                // double the move distance
+                Boost = 50f;
+                Boost = Mathf.Clamp(Boost, 0, 100f);
+            }
+
+            if (isBoosting == false) Boost = 0f;
+            rb.AddRelativeForce(0, 0, Boost);
+        }
+    }
+}
--- Assets/Team members/Tim/scripts/Test Scripts/SprinterAI.cs
+++ Assets/Team members/Tim/scripts/Test Scripts/SprinterAI.cs
@@ -1,27 +1,41 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using Anthill.AI;
+﻿using Anthill.AI;
 using UnityEngine;
 
 namespace TimPearson
 {
-    
     public class SprinterAI : MonoBehaviour
     {
         public Target target;
         public AntAIAgent antAIAgent;
-            
+        public Rigidbody rb;
+        public LayerMask RaycastHitLayer;
+        private Sprint sprint;
+
         // Start is called before the first frame update
-        void Start()
+        private void Start()
         {
             if (!(antAIAgent is null)) antAIAgent.SetGoal("At Position");
+            sprint = GetComponent<Sprint>();
         }
 
         // Update is called once per frame
-        void Update()
+        private void Update()
         {
-        
+            if (target != null)
+            {
+                var ray = new Ray(transform.position, transform.forward);
+                RaycastHit raycastBoost;
+                if (Physics.Raycast(ray, out raycastBoost, 500, RaycastHitLayer))
+                {
+                    Debug.DrawLine(ray.origin, raycastBoost.point, Color.green);
+                    sprint.isBoosting = true;
+                }
+                else
+                {
+                    Debug.DrawLine(transform.position, target.transform.position);
+                    sprint.isBoosting = false;
+                }
+            }
         }
     }
-
 }
