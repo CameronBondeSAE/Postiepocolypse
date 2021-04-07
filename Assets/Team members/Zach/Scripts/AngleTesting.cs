89d99a2ef6e73acaf6b1957ab030150ec2fd962e
Zachariah <1019559@student.sae.edu.au>
2021-04-07 15:52:41 +1000

    SignedAngle and Angle Scripts and More gamemanager stuff

---
+++ Assets/Team members/Zach/Scripts/AngleTesting.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace ZachFrench
+{
+    
+    public class AngleTesting : MonoBehaviour
+    {
+        public GameObject target;
+        public Vector3 TargetAngle;
+        public float angle;
+        public Rigidbody rb;
+        public float speed = 10;
+
+        // Start is called before the first frame update
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            TargetAngle = target.transform.position - transform.position;
+            angle = Vector3.Angle(TargetAngle, transform.forward);
+
+            rb.velocity = TargetAngle.normalized * speed;
+            
+            
+            Debug.DrawLine(transform.position, target.transform.position);
+        }
+    }
+}
---
+++ Assets/Team members/Zach/Scripts/SignedAngle.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SignedAngle : MonoBehaviour
+{
+    public GameObject target;
+    public float angle;
+    public Vector3 targetAngle;
+    public Vector3 forward;
+    public Rigidbody rb;
+    public float speed = 8;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //assigning the distance from target to current location
+        targetAngle = target.transform.position - transform.position;
+        //setting up a forward vector 
+        forward = Vector3.forward;
+        //signed angle uses the distance from target and the forward of the object to calculate the angle 
+        angle = Vector3.SignedAngle(targetAngle, forward, Vector3.up);
+
+        rb.velocity = targetAngle.normalized * speed;
+        
+        Debug.DrawLine(transform.position, target.transform.position);
+    }
+}
