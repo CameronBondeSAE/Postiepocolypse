1cfab34c1741bd05d6a9027bf19f512437b1a8e4
Zachariah <1019559@student.sae.edu.au>
2021-04-08 11:53:02 +1000

    Edits of GameMode Manager

--- Assets/Team members/Zach/Scripts/GamemodeManager.cs
+++ Assets/Team members/Zach/Scripts/GamemodeManager.cs
@@ -14,11 +14,11 @@ namespace ZachFrench
         // Make sure that there are already the other managers in the scene as these only grab them and don't spawn them
         private RespawnManager rm;
         private ResourceSpawner rs;
-        private Luke.Spawner hellSpawner;
+        private Luke.Spawner portalSpawner;
         //These are just set values for testing and can be altered in code or in the scene 
         public int civilianCount = 5;
         public int playerCount = 2;
-        public int creatureCount = 5;
+        public int portalCount = 3;
 
         // Start is called before the first frame update
         void Awake()
@@ -26,12 +26,15 @@ namespace ZachFrench
             //Finds the other managers 
             rm = FindObjectOfType<RespawnManager>();
             rs = FindObjectOfType<ResourceSpawner>();
-            hellSpawner = FindObjectOfType<Luke.Spawner>();
-            
+            portalSpawner = FindObjectOfType<Luke.Spawner>();
+          
             //this sets the values within the managers to the values located in the Gamemode Manager
+            //first is the respawn manager 
             rm.numberOfCivilian = civilianCount;
             rm.numberOfPlayers = playerCount;
-            hellSpawner.numberOfCreatures = creatureCount;
+            //hellspawner is next 
+            portalSpawner.numberOfPrefabs = portalCount;
+            
         }
 
         // Update is called once per frame
