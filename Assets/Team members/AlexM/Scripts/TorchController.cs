d8b3db27da36a0c52cec9a9e488b7bb3ae9a2e14
AJ <1018212@student.sae.edu.au>
2021-04-08 18:25:50 +1000

    whoopsies

--- Assets/Team members/AlexM/Scripts/TorchController.cs
+++ Assets/Team members/AlexM/Scripts/TorchController.cs
@@ -4,47 +4,56 @@ using System.Collections.Generic;
 using Mirror;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using Random = UnityEngine.Random;
 
 public class TorchController : NetworkBehaviour
 {
     public Light flashlight;
+    public bool lightOn = false;
+    
+    
 
-
-    public void Awake()
-    {
-        flashlight = GetComponent<Light>();
-    }
-
+    // We need to make this ONLY run on the player you own, not ALL players on the clients
     public void FlashLightInput(InputAction.CallbackContext obj)
     {
-        //if (isLocalPlayer)
+        if (isLocalPlayer)
         {
             if (obj.performed)
             {
-                ToggleLight();
+                CmdToggleLight();
             }
         }
     }
-    void ToggleLight()
+
+// Command function. Client to Server, telling it you changed the state of the torch
+    [Command]
+    void CmdToggleLight()
     {
-        //if (isServer)
+        
+        Debug.Log("Flashlight button pressed");
+        
+        if (lightOn)
         {
-            if (flashlight == null)
-            {
-                Debug.Log("Please add the players light source to Cam_MouseLook");
-                return;
-            }
-            
-            Debug.Log("Flashlight button pressed");
-            
-            if (flashlight.enabled)
-            {
-                flashlight.enabled = false;
-            }
-            else if (!flashlight.enabled)
-            {
-                flashlight.enabled = true;
-            }
+            lightOn = false;
         }
+        else if (!lightOn)
+        {
+            lightOn = true;
+        }
+        
+        RpcSetTorchState(lightOn);
+    }
+
+    [ClientRpc]
+    public void RpcSetTorchState(bool state)
+    {
+        if (flashlight == null)
+        {
+            Debug.Log("Please add the players light source to Cam_MouseLook");
+            return;
+        }
+        flashlight.enabled = state;
     }
 }
+
+
