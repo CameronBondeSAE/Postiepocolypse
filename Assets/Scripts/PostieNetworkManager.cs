f219eadeb37b28976a6094c9727341b8e7d0fc02
Cameron Bonde <c.bonde@sae.edu>
2021-04-08 18:16:58 +1000

    Events for Network manager

--- Assets/Scripts/PostieNetworkManager.cs
+++ Assets/Scripts/PostieNetworkManager.cs
@@ -1,15 +1,28 @@
 ﻿using Mirror;
+using System;
 using System.Collections.Generic;
+using Tanks;
 using UnityEngine;
 
 public class PostieNetworkManager : NetworkManager
 {
-	public List<GameObject> players;
+	public List<GameObject>         players;
+	public event Action<GameObject> newPlayerEvent;
+	public event Action<GameObject> playerDisconnectedEvent;
 	
 	public override void OnServerAddPlayer(NetworkConnection conn)
 	{
 		base.OnServerAddPlayer(conn);
 		
 		players.Add(conn.identity.gameObject);
+		newPlayerEvent?.Invoke(conn.identity.gameObject);
+	}
+
+	public override void OnServerDisconnect(NetworkConnection conn)
+	{
+		base.OnServerDisconnect(conn);
+
+		players.Remove(conn.identity.gameObject);
+		playerDisconnectedEvent?.Invoke(conn.identity.gameObject);
 	}
 }
