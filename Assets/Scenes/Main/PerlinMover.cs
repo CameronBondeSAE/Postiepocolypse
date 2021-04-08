255e91a9fef8636b49386e9b8e0c5464304861a1
Cameron Bonde <c.bonde@sae.edu>
2021-04-08 12:01:12 +1000

    Renamed TestCam to MAIN
    Added creatures to Portal prefab

---
+++ Assets/Scenes/Main/PerlinMover.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PerlinMover : MonoBehaviour
+{
+	public Vector3 scale     = Vector3.one;
+	public float   timeScale = 1;
+	Vector3        startingPosition;
+
+	// Start is called before the first frame update
+	void Start()
+	{
+		startingPosition = transform.position;
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+		float x = Mathf.PerlinNoise(Time.time * timeScale, startingPosition.x) * scale.x;
+		float y = Mathf.PerlinNoise(Time.time * timeScale, startingPosition.y) * scale.y;
+		float z = Mathf.PerlinNoise(Time.time * timeScale, startingPosition.z) * scale.z;
+
+		transform.position = startingPosition + new Vector3(x, y, z);
+	}
+}
