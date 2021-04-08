df95c612af364270639c15a09f851a706f0c5164
Cameron Bonde <c.bonde@sae.edu>
2021-04-08 18:02:00 +1000

    Resource spawns

---
+++ Assets/Scripts/PostieNetworkManager.cs
@@ -0,0 +1,15 @@
+﻿using Mirror;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PostieNetworkManager : NetworkManager
+{
+	public List<GameObject> players;
+	
+	public override void OnServerAddPlayer(NetworkConnection conn)
+	{
+		base.OnServerAddPlayer(conn);
+		
+		players.Add(conn.identity.gameObject);
+	}
+}
