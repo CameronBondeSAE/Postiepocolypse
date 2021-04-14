f95ebf53c04eeec7169888fd21fc2cfa7d2f6c6d
Damien Harwood <1019243@student.sae.edu.au>
2021-04-14 15:02:52 +1000

    Fixed a couple of things

---
+++ Assets/Team members/Damien/Scripts/LightController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightController : MonoBehaviour
+{
+    public Light light;
+
+    public bool flashOn;
+    float startTime = 0;
+    private float waitForSeconds = 5f;
+
+    private void Start()
+    {
+        
+    }
+
+    private void Update()
+    {
+        if (flashOn)
+        {
+            light.intensity = 200000000f;
+            if (flashOn && Time.time - startTime > waitForSeconds)
+            {
+                flashOn = false;
+            }
+        }
+
+        if (!flashOn)
+        {
+            light.intensity = 0f;
+        }
+    }
+}
