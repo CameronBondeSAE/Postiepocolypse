dfab7fe86d12eac2e0fb6b5516d11525240e995a
Riley McGowan <1017093@student.sae.edu.au>
2021-04-15 11:26:04 +1000

    Target Error Fix

--- Assets/Team members/Riley/Scripts/RunTarget.cs
+++ Assets/Team members/Riley/Scripts/RunTarget.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class RunTarget : FakeTarget
+public class RunTarget : WaterTarget
 {
     // Start is called before the first frame update
     void Start()
