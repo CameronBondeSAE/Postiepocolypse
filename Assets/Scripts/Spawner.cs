d594a5e9ea6e5dcc9e5c7a79b13731334b5d0b3f
Luke <1019817@student.sae.edu.au>
2021-04-15 12:06:15 +1000

    better spawner script

--- Assets/Scripts/Spawner.cs
+++ Assets/Scripts/Spawner.cs
@@ -17,11 +17,13 @@ namespace Luke
         [Header("Spawn Ray-casting")]
         public float ySpawnGroundOffset = 1f;
         public float yAdjustablePosition;
+        public LayerMask groundLayer;
+        public float yGroundTestOffset = 5f;
         public void Start()
         {
             if (spawnOnStart)
             {
-                SpawnSetPrefabs();
+                SpawnMultipleSets();
             }
         }
 
@@ -29,57 +31,63 @@ namespace Luke
         /// TODO: Need to figure out how to have the spawn ignore everything except the ground and also the option to part the spawns further away.
         /// </summary>
         //spawn with the amount numberOfPrefabs with a range of different types of prefabs
-        public void SpawnSetPrefabs()
+        public void SpawnMultipleSets()
         {
-            GameObject currentPrefab = new GameObject();
-            
-            //position at each spawn point
+            //multiple sets of prefabs
             for (int i = 0; i < setsOfPrefabs; i++)
             {
-                //hard coded height for the raycast to the ground
-                Vector3 position = transform.position + new Vector3(Random.Range(-spawnRange, spawnRange),
-                    transform.position.y + yAdjustablePosition, Random.Range(-spawnRange, spawnRange));
+                SpawnSetPrefab();
+            }
+        }
 
-                currentPrefab = SpawnPrefab(currentPrefab, position);
+        public void SpawnSetPrefab()
+        {
+            //spawn a single set
+            for (int j = 0; j < prefabs.Length; j++)
+            {
+                SpawnSingle(prefabs[j]);
             }
         }
 
         /// <summary>
         /// Use this function if you want to call this functionality for a single prefab
         /// </summary>
-        public GameObject SpawnPrefab(GameObject currentPrefab, Vector3 position)
+        public GameObject SpawnSingle(GameObject prefab)
         {
-            //loop through prefabs list
-            for (int j = 0; j < prefabs.Length; j++)
-            {
-                //set current random on the loop
-                int randomPrefab = Random.Range(0, prefabs.Length);
+            GameObject spawnedInstance;
+            //set current random on the loop
+            int randomPrefab = Random.Range(0, prefabs.Length);
 
-                if (randomSpawn)
-                {
-                    currentPrefab = Instantiate(prefabs[randomPrefab], position, Quaternion.identity);
-                }
-                else
-                {
-                    currentPrefab = Instantiate(prefabs[j], position, Quaternion.identity);
-                }
+            //random position for spawn + an adjustable y position in case of very large prefabs
+            Vector3 position = transform.position + new Vector3(Random.Range(-spawnRange, spawnRange),
+                transform.position.y + yAdjustablePosition, Random.Range(-spawnRange, spawnRange));
+            
+            if (randomSpawn)
+            {
+                spawnedInstance = Instantiate(prefabs[randomPrefab], position, transform.rotation);
+            }
+            else
+            {
+                spawnedInstance = Instantiate(prefab, position, transform.rotation);
             }
 
             //raycast hit spawn pos
             RaycastHit hitInfo;
-            Physics.Raycast(new Ray(position, -transform.up), out hitInfo);
+            Physics.Raycast(new Ray(position + new Vector3(0,yGroundTestOffset,0),
+                -transform.up), out hitInfo);
             if (hitInfo.collider)
             {
-                currentPrefab.transform.position = hitInfo.point + new Vector3(0, ySpawnGroundOffset, 0);
+                spawnedInstance.transform.position = hitInfo.point + new Vector3(0, ySpawnGroundOffset, 0);
+                Debug.Log(hitInfo.collider.gameObject.name);
             }
             //just in case the prefabs spawn in the ground
             else
             {
-                Destroy(currentPrefab);
+                Destroy(spawnedInstance);
                 Debug.Log("Failed Spawn");
             }
-
-            return currentPrefab;
+            ///TODO: Keep on trying to find a surface instead of deleting
+            return spawnedInstance;
         }
     }
 }
