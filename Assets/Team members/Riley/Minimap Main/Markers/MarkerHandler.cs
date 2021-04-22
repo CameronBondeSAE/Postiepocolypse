3459c68324b148195654d3feb99110339c0810a5
Riley <1017093@student.sae.edu.au>
2021-04-22 13:00:22 +1000

    Minimap Radar Push

---
+++ Assets/Team members/Riley/Minimap Main/Markers/MarkerHandler.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MarkerHandler : MonoBehaviour
+{
+    public int timeToLive;
+    private void Start()
+    {
+        StartCoroutine(MarkerDelete(timeToLive));
+    }
+
+    public IEnumerator MarkerDelete(int timeToLive)
+    {
+        yield return new WaitForSeconds(timeToLive);
+        Destroy(this.gameObject);
+    }
+}
