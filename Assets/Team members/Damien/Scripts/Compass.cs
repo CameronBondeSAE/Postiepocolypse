7816d6e687ae0f7bdcd5948afe7aa964e260ad7b
Damien <damien.harwood@Hotmail.com>
2021-04-22 12:25:23 +1000

    Added Compass to player

---
+++ Assets/Team members/Damien/Scripts/Compass.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Compass : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject compassNeedle;
+
+    // Update is called once per frame
+    void Update()
+    {
+        compassNeedle.transform.rotation = Quaternion.Euler(0, 0, 0);
+    }
+}
