e7a8a3738023e068435e193820f02261ba915445
slexx <slex-@hotmail.com>
2021-04-22 16:54:00 +1000

    save to work from laptop

---
+++ Assets/Team members/AlexM/Scripts/Threading.cs
@@ -0,0 +1,44 @@
+﻿using System.Threading;
+using Unity.Jobs;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.LowLevel;
+
+namespace AlexM
+{
+    public class Threading : MonoBehaviour
+    {
+        
+        
+        void Update()
+        {
+            if (InputSystem.GetDevice<Keyboard>().spaceKey.IsPressed())    
+            {
+                Thread myThread = new Thread(DoTheThing);
+                myThread.Start();
+            }
+
+            if (InputSystem.GetDevice<Keyboard>().jKey.IsPressed())
+            {
+                ThreadingJobs job = new ThreadingJobs();
+                job.Schedule();
+            }
+        }
+
+        private void DoTheThing(object obj)
+        {
+            while (true)    
+            {
+                Thread.Sleep(1000);
+                Debug.Log("A Wonderful day for a walk");
+                Thread.Sleep(2500);
+                Debug.Log("Whats that in the sky?...");
+                Thread.Sleep(1500);
+                Debug.Log("Oh Shi..");
+                Thread.Sleep(2500);
+                Debug.Log("I'm Dead.. X.X");
+            }
+        
+        }
+    }
+}
