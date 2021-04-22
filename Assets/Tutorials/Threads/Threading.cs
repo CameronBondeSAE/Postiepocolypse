dedd536aa5e661c645a221d452ceacb95f615144
Cam <c.bonde@sae.edu>
2021-04-22 16:56:06 +1000

    Main map layers etc Minimap included

--- Assets/Tutorials/Threads/Threading.cs
+++ Assets/Tutorials/Threads/Threading.cs
@@ -1,24 +1,56 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Threading;
+using Unity.Jobs;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class Threading : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private void Start()
     {
-        Thread thread = new Thread(DoStuff);
-        thread.Start();
+        for (int i = 0; i < 100; i++)
+        {
+            CSharpJob job = new CSharpJob();
+            JobHandle jobHandle = job.Schedule();
+        }
+
+        // for (int i = 0; i < 100; i++)
+        // {
+        //     Thread thread = new Thread(NormalThreads);
+        //     thread.Start();
+        // }
+    }
+
+
+    // Start is called before the first frame update
+    void Update()
+    {
+        if (InputSystem.GetDevice<Keyboard>().spaceKey.isPressed)
+        {
+            Thread thread = new Thread(NormalThreads);
+            thread.Start();
+        }
+
+        JobHandle jobHandle;
+        if (InputSystem.GetDevice<Keyboard>().jKey.isPressed)
+        {
+            CSharpJob job = new CSharpJob();
+            jobHandle = job.Schedule();
+        }
     }
 
     // This is running in a thread
-    private void DoStuff()
+    private void NormalThreads()
     {
-        Debug.Log("I'm waking up!");
-        Thread.Sleep(4000);
-        Debug.Log("I'm alive!");
-        Thread.Sleep(4000);
-        Debug.Log("I'm dead!");
+        float answer = 0;
+
+        for (int i = 0; i < 10000000; i++)
+        {
+            answer+=Mathf.Sqrt(i)+Mathf.PerlinNoise(i*1.24f,0);
+        }
+    
+        Debug.Log("I did something! : "+answer);
     }
 }
