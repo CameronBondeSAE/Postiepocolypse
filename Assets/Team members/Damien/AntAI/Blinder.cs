2edc66aeda0f397a945d8840291303199e442f0e
Damien Harwood <1019243@student.sae.edu.au>
2021-04-28 01:04:49 +1000

    Blinder AI scripts update

--- Assets/Team members/Damien/AntAI/Blinder.cs
+++ Assets/Team members/Damien/AntAI/Blinder.cs
@@ -9,6 +9,7 @@ namespace Damien
         public AntAIAgent antAIAgent;
         public GameObject target;
         
+        
         // Start is called before the first frame update
         void Start()
         {
