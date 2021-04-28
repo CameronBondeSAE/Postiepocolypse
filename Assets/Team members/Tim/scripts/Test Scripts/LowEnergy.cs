ad11e2fe296dcb71ba6de9d98d272ee63c5522d5
tim <1018360@student.sae.edu.au>
2021-04-28 16:18:19 +1000

    created low energy state for sprinter

---
+++ Assets/Team members/Tim/scripts/Test Scripts/LowEnergy.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Anthill.AI;
+using Tanks;
+using UnityEngine;
+using UnityEngine.AI;
+
+namespace TimPearson
+{
+    public class LowEnergy : AntAIState
+    {
+        public GameObject parent;
+        public SprintSense sprintSense;
+        private NavMeshAgent NavMeshAgent;
+        private Energy energy;
+        public override void Create(GameObject aGameObject)
+        {
+            base.Create(aGameObject);
+            parent = aGameObject;
+            sprintSense = parent.GetComponent<SprintSense>();
+            NavMeshAgent = parent.GetComponent<NavMeshAgent>();
+            energy = parent.GetComponent<Energy>();
+        }
+
+        public override void Enter()
+        {
+            base.Enter();
+            Debug.Log("Low Energy");
+            NavMeshAgent.SetDestination(transform.position);
+            //parent.transform.position = parent.transform.position;
+        }
+
+        public override void Execute(float aDeltaTime, float aTimeScale)
+        {
+            base.Execute(aDeltaTime, aTimeScale);
+            if (energy.CurrentAmount > 10f)
+            {
+                Finish();
+            }
+        }
+    }
+}
+
