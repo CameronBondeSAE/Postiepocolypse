f42b273b4d0ff5afd1e904f5839ab695784512d6
1018360 <1018360@student.sae.edu.au>
2021-05-05 16:52:10 +1000

    fixed hearing so it now registers ears

--- Assets/Team members/Tim/scripts/Test Scripts/SoundEmitter.cs
+++ Assets/Team members/Tim/scripts/Test Scripts/SoundEmitter.cs
@@ -12,12 +12,14 @@ namespace TimPearson
         private SphereCollider soundLevel3;
         public float soundRadius1;
         public List<GameObject> listOfEars = new List<GameObject>();
+        private Ears ears;
 
         private void Awake()
         {
             soundLevel1 = gameObject.AddComponent<SphereCollider>();
             soundLevel1.isTrigger = true;
             soundRadius1 = 10;
+            ears = GetComponent<Ears>();
         }
 
         private void Update()
@@ -25,19 +27,27 @@ namespace TimPearson
             soundLevel1.radius = soundRadius1;
         }
 
-        private void OnTriggerEnter(Collider soundLevel1)
+        private void OnTriggerEnter(Collider other)
         {
-            Ears ears = soundLevel1.GetComponent<Ears>();
+            ears = other.GetComponent<Ears>();
             if (ears)
             {
-                ears.Hearing(soundLevel1.transform.position);
-                listOfEars.Add(soundLevel1.gameObject);
+                listOfEars.Add(ears.gameObject);
+                //ears = null;
             }
         }
 
         private void OnTriggerExit(Collider soundLevel1)
         {
-            listOfEars.Remove(soundLevel1.gameObject);
+            listOfEars.Remove(ears.gameObject);
+        }
+
+        public void EmitSound(Vector3 sourcePosition)
+        {
+            foreach (var ears in listOfEars)
+            {
+                this.ears.Hearing(sourcePosition);
+            }
         }
     }
 }
