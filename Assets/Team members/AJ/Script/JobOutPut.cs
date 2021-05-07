62d17716ae0ff46473fde67108ee7736fac4737d
AJ <1018212@student.sae.edu.au>
2021-05-07 20:19:45 +1000

    Job done

---
+++ Assets/Team members/AJ/Script/JobOutPut.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Unity.Collections;
+using Unity.Jobs;
+using UnityEngine;
+
+public class JobOutPut : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        NativeArray<int> result = new NativeArray<int>(1, Allocator.TempJob);
+        SimpleJob simpleJob = new SimpleJob
+        {
+            a = 1,
+            b = 2,
+            result = result,
+        };
+        JobHandle jobHandle = simpleJob.Schedule();
+        jobHandle.Complete();
+        Debug.Log(simpleJob.result[0]);
+
+        result.Dispose();
+    }
+
+    public struct SimpleJob : IJob
+    {
+        public int a;
+        public int b;
+        public NativeArray<int> result;
+
+        public void Execute()
+        {
+            result[0] = a + b;
+        }
+    }
+}
